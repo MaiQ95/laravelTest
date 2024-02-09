e402dc5403eacd438dbf4ed1998e669148bbb08c
MaiQ95 <pawel19x95@gmail.com>
2024-02-09 16:11:55 +0100

    install breeze:livewire and spatie

--- config/app.php
+++ config/app.php
@@ -168,6 +168,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\VoltServiceProvider::class,
+        Spatie\Permission\PermissionServiceProvider::class,
     ])->toArray(),
 
     /*
